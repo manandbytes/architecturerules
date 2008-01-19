dc4c65b3ed6983369d75e52508c04b4a56b4cba3
mikenereson <mikenereson@fa5e1c15-7635-0410-8054-0fc223291177>
2008-01-19 22:20:45 +0000

    [maven-scm] copy for tag architecture-rules-2.0.3

---
+++ tags/architecture-rules-2.0.3/src/main/java/com/seventytwomiles/architecturerules/services/CyclicRedundancyServiceImpl.java
@@ -0,0 +1,230 @@
+package com.seventytwomiles.architecturerules.services;
+
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information visit
+ * http://architecturerules.googlecode.com/svn/docs/index.html
+ */
+
+
+import com.seventytwomiles.architecturerules.configuration.Configuration;
+import com.seventytwomiles.architecturerules.exceptions.CyclicRedundancyException;
+import com.seventytwomiles.architecturerules.exceptions.NoPackagesFoundException;
+import com.seventytwomiles.architecturerules.exceptions.SourceNotFoundException;
+import jdepend.framework.JavaPackage;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.*;
+
+
+
+/**
+ * <p>Checks for cyclic redundancy among application packages in the source
+ * folders.</p>
+ *
+ * @author mikenereson
+ * @see AbstractArchitecturalRules
+ */
+public class CyclicRedundancyServiceImpl extends AbstractArchitecturalRules
+        implements CyclicRedundancyService {
+
+
+    /**
+     * <p>Log to log with</p>
+     *
+     * @parameter log Log
+     */
+    private static final Log log = LogFactory.getLog(
+            CyclicRedundancyServiceImpl.class);
+
+
+    /**
+     * <p>Constructor instantiates a new <code>CyclicRedundancyService</code></p>
+     *
+     * @param configuration Configuration which contains the source directories
+     * to inspect
+     * @throws SourceNotFoundException when an required source directory does
+     * not exist and when <tt>exception</tt>=<tt>"true"</tt> in the source
+     * configuration
+     * @throws NoPackagesFoundException when none of the source directories
+     * exist and <tt>no-packages</tt>="<tt>ignore</tt>" in the sources
+     * configuration
+     */
+    public CyclicRedundancyServiceImpl(final Configuration configuration) {
+
+        super(configuration);
+
+        log.info("instantiating new CyclicRedundancyService");
+    }
+
+    // --------------------- Interface CyclicRedundancyService ---------------------
+
+
+    /**
+     * <p>Check all the packages in all of the source directories and search for
+     * any cyclic redundancy</p>
+     */
+    public void performCyclicRedundancyCheck() {
+
+        log.info("cyclic redundancy check requested");
+
+        final Collection packages = getPackages();
+
+        /**
+         *  TODO: report classes involved
+         *  Report on which class is causing the cyclic redundancy.
+         *  The information is all available from within the JavaPackage
+         *  class.
+         */
+
+        /**
+         * Holds any cycles that are found. The structure of this Map is
+         * <JavaPackage, Set> where the first String is the name of the package,
+         * and the Set contains JavaPackge of packages that are in a cycle with
+         * the named package.
+         */
+        final Map cycles = new HashMap();
+
+        JavaPackage javaPackage; /* place holder to iterate over */
+
+        for (final Iterator packageIterator = packages.iterator();
+             packageIterator.hasNext();) {
+
+            javaPackage = (JavaPackage) packageIterator.next();
+
+            final Collection afferents = javaPackage.getAfferents();
+            final Collection efferents = javaPackage.getEfferents();
+
+            /**
+             * afferents Collection is no longer a reference to the afferents,
+             * but now a Collection of packages involved in a cyclic dependency
+             * with the javaPackage. By calling retainAll the afferents
+             * Collection now contains only those javaPackages that were in both
+             * the efferents and afferents Collections. And by definition, when
+             * a package is both uses the given package, and used by the given
+             * package, a cyclic dependency exists.
+             */
+            afferents.retainAll(efferents);
+
+            final boolean cyclesFound = afferents.size() > 0;
+
+            if (cyclesFound)
+                addCycle(cycles, javaPackage, afferents);
+        }
+
+        if (cycles.isEmpty()) {
+
+            log.info("found no cyclic redundancies");
+
+        } else {
+
+            log.warn("found " + cycles.size() + " cyclic redundancies");
+
+            throw new CyclicRedundancyException(
+                    buildCyclicRedundancyMessage(cycles));
+        }
+
+        log.info("cyclic redundancy test completed");
+    }
+
+
+    /**
+     * <p>Updates a Map, or puts a new record into a Map of a JavaPackage and
+     * its cyclic dependency packages.</p>
+     *
+     * @param cycles Map of cycles already discovered.
+     * @param javaPackage JavaPackage involved in a cyclic dependency
+     * @param dependencies Collection of JavaPackages involved in a cyclic
+     * dependency with the given javaPackage argument.
+     */
+    private void addCycle(final Map cycles, final JavaPackage javaPackage,
+                          final Collection dependencies) {
+
+        final boolean exists = cycles.containsKey(javaPackage);
+        final Set cyclicPackages;
+
+        if (exists) {
+            /**
+             * Get existing Set, this JavaPackage has already been
+             * discovered for being in a cycle
+             */
+            final Object value = cycles.get(javaPackage);
+            cyclicPackages = (HashSet) value;
+
+        } else {
+
+            /**
+             * Build a new Set
+             */
+            cyclicPackages = new HashSet();
+        }
+
+        cyclicPackages.addAll(dependencies);
+        cycles.put(javaPackage, cyclicPackages);
+    }
+
+
+    /**
+     * <p>Builds a message detailing all of the java packages that are involved
+     * in a cyclic dependency and the packages involved with.</p>
+     *
+     * @param cycles Map of cycles discovered.
+     * @return String a complete message detailing all of the cyclic
+     *         dependencies found.
+     */
+    private String buildCyclicRedundancyMessage(final Map cycles) {
+
+        final StringBuffer message = new StringBuffer();
+        message.append("cyclic dependencies found:")
+                .append("\r\n")
+                .append("\r\n\t");
+
+        final Iterator entryIterator = cycles.entrySet().iterator();
+
+        while (entryIterator.hasNext()) {
+
+            final Map.Entry entry = (Map.Entry) entryIterator.next();
+
+            final JavaPackage javaPackage = (JavaPackage) entry.getKey();
+            final Set cyclicDependencies = (HashSet) entry.getValue();
+
+            message.append("-- ")
+                    .append(javaPackage.getName())
+                    .append("\r\n\t");
+
+            for (Iterator dependencyIterator = cyclicDependencies.iterator();
+                 dependencyIterator.hasNext();) {
+
+                final JavaPackage dependency
+                        = (JavaPackage) dependencyIterator.next();
+
+                message.append("¦  ¦").append("\r\n\t");
+                message.append("¦  ¦-- ")
+                        .append(dependency.getName())
+                        .append("\r\n\t");
+
+                if (!dependencyIterator.hasNext())
+                    message.append("¦").append("\r\n\t");
+            }
+
+            if (entryIterator.hasNext())
+                message.append("¦").append("\r\n\t");
+        }
+
+        return message.toString();
+    }
+}
---
+++ tags/architecture-rules-2.0.3/src/test/java/com/seventytwomiles/architecturerules/domain/SourceDirectoryTest.java
@@ -0,0 +1,374 @@
+package com.seventytwomiles.architecturerules.domain;
+
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information visit
+ * http://architecturerules.googlecode.com/svn/docs/index.html
+ */
+
+
+import junit.framework.TestCase;
+
+import java.io.File;
+
+
+
+/**
+ * <p><code>SourceDirectory</code> Tester.</p>
+ *
+ * @author mikenereson
+ */
+public class SourceDirectoryTest extends TestCase {
+
+
+    /**
+     * <p>Instance of a sourceDirectory to test against.</p>
+     */
+    private SourceDirectory sourceDirectory;
+
+
+    public SourceDirectoryTest(String name) {
+        super(name);
+    }
+
+
+    /**
+     * @see TestCase#setName(String)
+     */
+    public void setUp() throws Exception {
+        super.setUp();
+
+        sourceDirectory = new SourceDirectory();
+    }
+
+
+    /**
+     * @see TestCase#tearDown()
+     */
+    public void tearDown() throws Exception {
+        sourceDirectory = null;
+
+        super.tearDown();
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#equals(Object)} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testEquals() throws Exception {
+        SourceDirectory that;
+
+        sourceDirectory.setPath("core/target/classes");
+        that = new SourceDirectory("core/target/classes");
+        assertTrue(sourceDirectory.equals(that));
+        assertTrue(sourceDirectory.hashCode() == that.hashCode());
+
+        that = new SourceDirectory("web/target/classes");
+        assertFalse(sourceDirectory.equals(that));
+        assertFalse(sourceDirectory.hashCode() == that.hashCode());
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#SourceDirectory(String)} and {@link
+     * SourceDirectory#SourceDirectory(String,boolean)} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testInterestingConstructors() throws Exception {
+
+        /**
+         * Test SourceDirectory#SourceDirectory(String)
+         */
+        sourceDirectory = new SourceDirectory("core/target/classes");
+        assertEquals(
+                "core" + File.separator + "target" + File.separator + "classes"
+                , sourceDirectory.getPath());
+
+        /**
+         * Test SourceDirectory#SourceDirectory(String, boolean)
+         */
+        sourceDirectory = new SourceDirectory("core/target/classes", true);
+        assertEquals(
+                "core" + File.separator + "target" + File.separator + "classes",
+                sourceDirectory.getPath());
+        assertTrue(sourceDirectory.shouldThrowExceptionWhenNotFound());
+
+        sourceDirectory = new SourceDirectory("core/target/classes", false);
+        assertEquals(
+                "core" + File.separator + "target" + File.separator + "classes",
+                sourceDirectory.getPath());
+        assertFalse(sourceDirectory.shouldThrowExceptionWhenNotFound());
+
+        /**
+         * Test SourceDirectory#SourceDirectory(String, String)
+         */
+        sourceDirectory = new SourceDirectory("core/target/classes", "ignore");
+        assertEquals(
+                "core" + File.separator + "target" + File.separator + "classes",
+                sourceDirectory.getPath());
+        assertFalse(sourceDirectory.shouldThrowExceptionWhenNotFound());
+
+        sourceDirectory = new SourceDirectory("core/target/classes",
+                "exception");
+        assertEquals(
+                "core" + File.separator + "target" + File.separator + "classes",
+                sourceDirectory.getPath());
+        assertTrue(sourceDirectory.shouldThrowExceptionWhenNotFound());
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#SourceDirectory(String)} and {@link
+     * SourceDirectory#SourceDirectory(String,boolean)} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testInterestingConstructors_illegalArguments()
+            throws Exception {
+
+        try {
+            sourceDirectory = new SourceDirectory("");
+            fail("expected IllegalArgumentException");
+        } catch (final Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+
+        try {
+            sourceDirectory = new SourceDirectory(null);
+            fail("expected IllegalArgumentException");
+        } catch (final Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+
+
+        try {
+            sourceDirectory = new SourceDirectory("", true);
+            fail("expected IllegalArgumentException");
+        } catch (final Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+
+        try {
+            sourceDirectory = new SourceDirectory(null, true);
+            fail("expected IllegalArgumentException");
+        } catch (final Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+
+        try {
+            sourceDirectory = new SourceDirectory("core/target/classes",
+                    "monkey");
+            fail("expected IllegalArgumentException");
+        } catch (final Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+
+
+        try {
+            sourceDirectory = new SourceDirectory("core/target/classes", "");
+            fail("expected IllegalArgumentException");
+        } catch (final Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+
+        try {
+            sourceDirectory = new SourceDirectory("core/target/classes", null);
+            fail("expected IllegalArgumentException");
+        } catch (final Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#setPath(String)} and {@link
+     * SourceDirectory#getPath()} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testSetGetPath_windows() throws Exception {
+
+        if (File.separator.equals("\\")) {
+
+            sourceDirectory.setPath("core/target/classes");
+            assertEquals("core\\target\\classes", sourceDirectory.getPath());
+
+            sourceDirectory.setPath("core\\target\\classes");
+            assertEquals("core\\target\\classes", sourceDirectory.getPath());
+        }
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#setPath(String)} and {@link
+     * SourceDirectory#getPath()} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testSetGetPath_unix() throws Exception {
+
+        if (File.separator.equals("/")) {
+
+            sourceDirectory.setPath("core/target/classes");
+            assertEquals("core/target/classes", sourceDirectory.getPath());
+
+            sourceDirectory.setPath("core\\target\\classes");
+            assertEquals("core/target/classes", sourceDirectory.getPath());
+        }
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#setPath(String)} and {@link
+     * SourceDirectory#getPath()} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testSetGetPath_illegalArguments() throws Exception {
+
+        sourceDirectory.setPath("core/target/classes");
+        assertEquals(
+                "core" + File.separator + "target" + File.separator + "classes",
+                sourceDirectory.getPath());
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#setNotFound(String)} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testSetNotFound() throws Exception {
+
+        /* check the initial state*/
+        assertFalse(sourceDirectory.shouldThrowExceptionWhenNotFound());
+
+        /* change value */
+        sourceDirectory.setNotFound("exception");
+
+        /* check ending state */
+        assertTrue(sourceDirectory.shouldThrowExceptionWhenNotFound());
+
+        /* check the initial state*/
+        assertTrue(sourceDirectory.shouldThrowExceptionWhenNotFound());
+
+        /* change value */
+        sourceDirectory.setNotFound("ignore");
+
+        /* check ending state */
+        assertFalse(sourceDirectory.shouldThrowExceptionWhenNotFound());
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#setNotFound(String)} when illegal
+     * arguments are passed</p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testSetNotFound_illegalArguments() throws Exception {
+
+        try {
+            sourceDirectory.setNotFound("");
+            fail("expected IllegalArgumentException");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+
+        try {
+            sourceDirectory.setNotFound(null);
+            fail("expected IllegalArgumentException");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+
+        try {
+            sourceDirectory.setNotFound("http://www.72miles.com");
+            fail("expected IllegalArgumentException");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#setShouldThrowExceptionWhenNotFound(boolean)}
+     * </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testSetShouldThrowExceptionWhenNotFound() throws Exception {
+
+        sourceDirectory.setShouldThrowExceptionWhenNotFound(true);
+        assertTrue(sourceDirectory.shouldThrowExceptionWhenNotFound());
+
+        sourceDirectory.setShouldThrowExceptionWhenNotFound(false);
+        assertFalse(sourceDirectory.shouldThrowExceptionWhenNotFound());
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#replaceBackslashForOS(String)} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testFixSlashForOS_windows() throws Exception {
+
+        if (File.separator.equals("\\")) {
+
+            assertEquals("src\\main\\resources",
+                    sourceDirectory.replaceBackslashForOS(
+                            "src\\main\\resources"));
+            assertEquals("src\\main\\resources",
+                    sourceDirectory.replaceBackslashForOS(
+                            "src/main/resources"));
+        }
+    }
+
+
+    /**
+     * <p>Test for {@link SourceDirectory#replaceBackslashForOS(String)} </p>
+     *
+     * @throws Exception when <code>SourceDirectory</code> throws and unexpected
+     * <code>Exception</code>
+     */
+    public void testFixSlashForOS_unix() throws Exception {
+
+        if (File.separator.equals("/")) {
+
+            assertEquals("src/main/resources",
+                    sourceDirectory.replaceBackslashForOS(
+                            "src\\main\\resources"));
+            assertEquals("src/main/resources",
+                    sourceDirectory.replaceBackslashForOS(
+                            "src/main/resources"));
+        }
+    }
+
+}
